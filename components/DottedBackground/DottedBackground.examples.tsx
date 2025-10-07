c6797b60c7636fbf0058cbed2140b64975963162
Lyco Angelo Ty <lyco@bluedroplabs.com>
2025-10-07 23:01:26 +0800

    feat: create DottedBackground component

---
+++ components/DottedBackground/DottedBackground.examples.tsx
@@ -0,0 +1,9 @@
+import type { HTMLAttributes } from "react";
+
+const DOTTED_BACKGROUND_DEFAULT_PROPS: HTMLAttributes<HTMLElement> = {
+  className: "h-screen flex pt-10 justify-center",
+};
+
+export const DOTTED_BACKGROUND_EXAMPLE_PROPS = {
+  default: DOTTED_BACKGROUND_DEFAULT_PROPS,
+};
