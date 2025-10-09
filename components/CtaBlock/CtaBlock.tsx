c9320f93d4a00fa229ff4db5d0122abcb4758aa0
Lyco Angelo Ty <lyco@bluedroplabs.com>
2025-10-09 22:13:41 +0800

    feat: restructure CtaBlock component layout and enhance styles for responsiveness

--- components/CtaBlock/CtaBlock.tsx
+++ components/CtaBlock/CtaBlock.tsx
@@ -6,9 +6,10 @@ import { Button } from "../Button/Button";
 import type { ICtaBlock } from "./CtaBlock.types";
 
 const styles = {
-  container: "font-medium py-20 text-center",
+  container: "font-medium text-center py-20 lg:py-30",
   cta: "mt-6",
   description: "font-light mt-4 text-default-base/90",
+  inner: "mx-auto lg:max-w-154.5",
   title: "leading-none text-default-heading text-size-48 tracking-[-0.02em]",
 };
 
@@ -21,9 +22,11 @@ export const CtaBlock = ({
 }: ICtaBlock) => {
   return (
     <Container className={cn(styles.container, className)} {...props}>
-      <h2 className={styles.title}>{title}</h2>
-      <p className={styles.description}>{description}</p>
-      <Button {...cta} className={styles.cta} />
+      <div className={styles.inner}>
+        <h2 className={styles.title}>{title}</h2>
+        <p className={styles.description}>{description}</p>
+        <Button {...cta} className={styles.cta} />
+      </div>
     </Container>
   );
 };
