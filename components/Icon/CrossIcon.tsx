53d578931421f2ff1f6ec3f864096dde6ab48c64
Lyco Angelo Ty <lyco@bluedroplabs.com>
2025-10-18 00:11:24 +0800

    feat(icons): Add CheckIcon and CrossIcon

---
+++ components/Icon/CrossIcon.tsx
@@ -0,0 +1,31 @@
+import type { SVGProps } from "react";
+
+export const CrossIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    fill="none"
+    viewBox="0 0 33 32"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <g clipPath="url(#clip0_242_9468)">
+      <path
+        d="M32.668 16C32.668 7.16344 25.5045 0 16.668 0C7.83141 0 0.667969 7.16344 0.667969 16C0.667969 24.8366 7.83141 32 16.668 32C25.5045 32 32.668 24.8366 32.668 16Z"
+        fill="currentColor"
+      />
+      <path
+        d="M11.0671 23.4524L9.21484 21.6001L14.8149 16.0001L9.21484 10.4001L11.0671 8.54785L16.6671 14.1479L22.2671 8.54785L24.1194 10.4001L18.5194 16.0001L24.1194 21.6001L22.2671 23.4524L16.6671 17.8524L11.0671 23.4524Z"
+        fill="#002024"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_242_9468">
+        <rect
+          fill="white"
+          height="32"
+          transform="translate(0.667969)"
+          width="32"
+        />
+      </clipPath>
+    </defs>
+  </svg>
+);
