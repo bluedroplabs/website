1a7ade0ba9659934df41261bbcf4890f32edef9b
Lyco Angelo Ty <lyco@bluedroplabs.com>
2025-10-28 22:10:51 +0800

    feat: Refactor CardGrid component structure

--- components/CardGrid/CardGrid.tsx
+++ components/CardGrid/CardGrid.tsx
@@ -77,65 +77,67 @@ export const CardGrid = ({
   }
 
   return (
-    <section className={className} {...props}>
-      <Container className="border-y border-border-normal max-md:px-0">
-        <div className="border-x border-border-normal flex gap-x-8 items-center justify-between px-5 py-4 lg:px-8 lg:py-6">
-          <SearchBar
-            className="w-full"
-            onSubmit={(e) => {
-              e.preventDefault();
-              setSearchTerm(e.currentTarget.search.value);
-            }}
-          />
-          {filters?.map((filter) => (
-            <MultiSelect
-              {...filter}
-              className="whitespace-nowrap max-md:hidden"
-              key={filter.name}
-              onChange={handleFilterChange(filter.name)}
+    <Container className={className} noPadding {...props}>
+      <section>
+        <Container className="border-y border-border-normal max-md:px-0">
+          <div className="border-x border-border-normal flex gap-x-8 items-center justify-between px-5 py-4 lg:px-8 lg:py-6">
+            <SearchBar
+              className="w-full"
+              onSubmit={(e) => {
+                e.preventDefault();
+                setSearchTerm(e.currentTarget.search.value);
+              }}
             />
-          ))}
-        </div>
-      </Container>
-      {groupedItems.map((group, groupIndex) => (
-        <Container
-          className="max-md:px-0 md:border-y md:border-border-normal"
-          key={groupIndex}
-        >
-          <div
-            className={cn(
-              "border-x border-border-normal lg:grid lg:grid-cols-3",
-            )}
-          >
-            {group.map((item, itemIndex) => {
-              const isFirst = groupIndex === 0;
-              const globalIndex =
-                groupIndex === 0 ? 0 : (groupIndex - 1) * 3 + itemIndex + 1;
-
-              return (
-                <ResourceCard
-                  {...item}
-                  className={cn(
-                    "border-border-normal max-md:border-t [&:nth-child(3n+2)]:lg:border-x",
-                    isFirst && "lg:col-span-3",
-                  )}
-                  key={globalIndex}
-                  variant={isFirst ? "featured" : "default"}
-                />
-              );
-            })}
+            {filters?.map((filter) => (
+              <MultiSelect
+                {...filter}
+                className="whitespace-nowrap max-md:hidden"
+                key={filter.name}
+                onChange={handleFilterChange(filter.name)}
+              />
+            ))}
           </div>
         </Container>
-      ))}
+        {groupedItems.map((group, groupIndex) => (
+          <Container
+            className="max-md:px-0 md:border-y md:border-border-normal"
+            key={groupIndex}
+          >
+            <div
+              className={cn(
+                "border-x border-border-normal lg:grid lg:grid-cols-3",
+              )}
+            >
+              {group.map((item, itemIndex) => {
+                const isFirst = groupIndex === 0;
+                const globalIndex =
+                  groupIndex === 0 ? 0 : (groupIndex - 1) * 3 + itemIndex + 1;
 
-      <Container className="border-border-normal max-md:border-t">
-        <Pagination
-          className="py-6 md:border-x md:border-border-normal"
-          currentPage={page}
-          onPageChange={handlePageChange}
-          totalPages={totalPages}
-        />
-      </Container>
-    </section>
+                return (
+                  <ResourceCard
+                    {...item}
+                    className={cn(
+                      "border-border-normal max-md:border-t [&:nth-child(3n+2)]:lg:border-x",
+                      isFirst && "lg:col-span-3",
+                    )}
+                    key={globalIndex}
+                    variant={isFirst ? "featured" : "default"}
+                  />
+                );
+              })}
+            </div>
+          </Container>
+        ))}
+
+        <Container className="border-border-normal max-md:border-t">
+          <Pagination
+            className="py-6 md:border-x md:border-border-normal"
+            currentPage={page}
+            onPageChange={handlePageChange}
+            totalPages={totalPages}
+          />
+        </Container>
+      </section>
+    </Container>
   );
 };
