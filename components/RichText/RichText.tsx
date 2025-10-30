2f506876dd6f20eb0ed4ca758333381c7a3c6fb7
Lyco Angelo Ty <lyco@bluedroplabs.com>
2025-10-30 09:13:06 +0800

    feat: Implement RichText component with default props and types

---
+++ components/RichText/RichText.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { cn } from "@/utils";
+import parse from "html-react-parser";
+import type { IRichText } from "./RichText.types";
+
+export const RichText = ({ className, content, ...props }: IRichText) => {
+  return (
+    <section
+      className={cn(
+        "space-y-3",
+        "[&_a]:text-brand-aqua [&_a]:underline [&_a]:hover:text-brand-light-blue",
+        "[&_strong]:font-semibold [&_b]:font-semibold [&_em]:italic [&_i]:italic",
+        "[&_ul]:list-disc [&_ul]:pl-5 [&_ul]:space-y-2",
+        "[&_ol]:list-decimal [&_ol]:pl-5 [&_ol]:space-y-2",
+        "[&_li]:leading-6",
+        "[&_h1]:text-size-24 lg:[&_h1]:text-size-40",
+        "[&_h2]:text-size-24 lg:[&_h2]:text-size-40",
+        "[&_h3]:text-size-20 lg:[&_h3]:text-size-24",
+        "[&_h4]:text-size-20 lg:[&_h4]:text-size-24",
+        "[&_h5]:text-size-20 lg:[&_h5]:text-size-24",
+        "[&_h6]:text-size-20 lg:[&_h6]:text-size-24",
+        "[&_p]:font-light [&_p]:text-size-18 lg:[&_p]:text-size-20",
+        "[&_blockquote]:border-l-2 [&_blockquote]:border-l-brand-light-blue [&_blockquote]:pl-4 [&_blockquote]:italic [&_blockquote]:my-8",
+        className,
+      )}
+      {...props}
+    >
+      {parse(content)}
+    </section>
+  );
+};
