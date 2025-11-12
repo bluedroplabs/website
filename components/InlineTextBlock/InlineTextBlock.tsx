586cf483eb612309af601b811331214d27128ab0
Lyco Angelo Ty <lyco@bluedroplabs.com>
2025-11-13 00:37:02 +0800

    fix: update InlineTextBlock container class and description className

--- components/InlineTextBlock/InlineTextBlock.tsx
+++ components/InlineTextBlock/InlineTextBlock.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Container } from "@/components/Container/Container";
+import { cn } from "@/utils";
 import { ContentBlock } from "../ContentBlock/ContentBlock";
 import type { IInlineTextBlock } from "./InlineTextBlock.types";
 
@@ -12,11 +13,14 @@ export const InlineTextBlock = ({
   ...props
 }: IInlineTextBlock) => {
   return (
-    <Container className={className} {...props}>
+    <Container
+      className={cn("border-t border-border-normal", className)}
+      {...props}
+    >
       <ContentBlock
         className="py-14 lg:border-x lg:border-border-normal lg:px-15 lg:py-20"
         description={description}
-        descriptionClassName="mt-5 [&_strong]:font-medium"
+        descriptionClassName="mb-8 mt-5 lg:mb-13 first:mt-0 last:mb-0 [&_strong]:font-medium"
         eyebrow={eyebrow}
         eyebrowClassName="mb-5 lg:mb-4"
         eyebrowVariant="highlight"
