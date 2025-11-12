e70aa940b843f67f53a265ee4ea11becb2db3ac3
Lyco Angelo Ty <lyco@bluedroplabs.com>
2025-11-13 00:02:05 +0800

    fix: add author and blockquote props to InlineTextBlock component

--- components/InlineTextBlock/InlineTextBlock.tsx
+++ components/InlineTextBlock/InlineTextBlock.tsx
@@ -5,22 +5,31 @@ import { ContentBlock } from "../ContentBlock/ContentBlock";
 import type { IInlineTextBlock } from "./InlineTextBlock.types";
 
 export const InlineTextBlock = ({
+  author,
+  blockquote,
   className,
   description,
   eyebrow,
+  primaryCTA,
   title,
   ...props
 }: IInlineTextBlock) => {
   return (
     <Container className={className} {...props}>
       <ContentBlock
+        author={author}
+        blockquote={blockquote}
+        blockquoteClassName="mt-6 lg:mt-auto"
         className="py-14 lg:border-x lg:border-border-normal lg:px-15 lg:py-20"
+        ctaGroupClassName="mt-auto"
         description={description}
-        descriptionClassName="mt-5 [&_strong]:font-medium"
+        descriptionClassName="mb-8 mt-5 lg:mb-13 last:mb-0 [&_strong]:font-medium"
         eyebrow={eyebrow}
         eyebrowClassName="mb-5 lg:mb-4"
         eyebrowVariant="highlight"
+        primaryCTA={primaryCTA}
         title={title}
+        titleClassName="lg:mb-6 last:lg:mb-0"
         variant="inline"
       />
     </Container>
