2de7ca8e8e1a9cd5d25917084a99f1d9f1297278
Ralph Largo <devlargs@gmail.com>
2026-02-19 19:34:16 +0800

    feat: update image on dark mode

--- components/LogoMarquee/LogoMarquee.examples.ts
+++ components/LogoMarquee/LogoMarquee.examples.ts
@@ -7,6 +7,7 @@ const LOGO_MARQUEE_DEFAULT_PROPS: ILogoMarquee = {
     {
       alt: "Client Logo 1",
       src: "https://picsum.photos/131/67",
+      srcLight: "https://picsum.photos/131/67-light",
       width: 131,
       height: 67,
       priority: true,
@@ -14,6 +15,7 @@ const LOGO_MARQUEE_DEFAULT_PROPS: ILogoMarquee = {
     {
       alt: "Client Logo 2",
       src: "https://picsum.photos/110/54",
+      srcLight: "https://picsum.photos/110/54-light",
       width: 110,
       height: 54,
       priority: true,
@@ -21,6 +23,7 @@ const LOGO_MARQUEE_DEFAULT_PROPS: ILogoMarquee = {
     {
       alt: "Client Logo 3",
       src: "https://picsum.photos/145/60",
+      srcLight: "https://picsum.photos/145/60-light",
       width: 145,
       height: 60,
       priority: true,
@@ -28,6 +31,7 @@ const LOGO_MARQUEE_DEFAULT_PROPS: ILogoMarquee = {
     {
       alt: "Client Logo 4",
       src: "https://picsum.photos/120/60",
+      srcLight: "https://picsum.photos/120/60-light",
       width: 120,
       height: 60,
       priority: true,
@@ -35,6 +39,7 @@ const LOGO_MARQUEE_DEFAULT_PROPS: ILogoMarquee = {
     {
       alt: "Client Logo 5",
       src: "https://picsum.photos/130/60",
+      srcLight: "https://picsum.photos/130/60-light",
       width: 130,
       height: 60,
       priority: true,
@@ -42,6 +47,7 @@ const LOGO_MARQUEE_DEFAULT_PROPS: ILogoMarquee = {
     {
       alt: "Client Logo 6",
       src: "https://picsum.photos/140/60",
+      srcLight: "https://picsum.photos/140/60-light",
       width: 140,
       height: 60,
       priority: true,
@@ -49,6 +55,7 @@ const LOGO_MARQUEE_DEFAULT_PROPS: ILogoMarquee = {
     {
       alt: "Client Logo 7",
       src: "https://picsum.photos/150/60",
+      srcLight: "https://picsum.photos/150/60-light",
       width: 150,
       height: 60,
       priority: true,
@@ -56,6 +63,7 @@ const LOGO_MARQUEE_DEFAULT_PROPS: ILogoMarquee = {
     {
       alt: "Client Logo 8",
       src: "https://picsum.photos/160/60",
+      srcLight: "https://picsum.photos/160/60-light",
       width: 160,
       height: 60,
       priority: true,
