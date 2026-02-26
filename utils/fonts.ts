aaa6d5d2414b0a11b324d63b256fab3549093eff
Ralph Largo <devlargs@gmail.com>
2026-02-27 00:35:07 +0800

    perf(fonts): optimize font loading to reduce LCP render delay

--- utils/fonts.ts
+++ utils/fonts.ts
@@ -2,12 +2,16 @@ import { Geist, Geist_Mono } from "next/font/google";
 
 export const geist = Geist({
   subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  weight: ["300", "400", "500", "600", "700"],
   variable: "--font-geist",
+  display: "swap",
+  preload: true,
 });
 
 export const geistMono = Geist_Mono({
   subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  weight: ["300", "400", "500", "600", "700"],
   variable: "--font-geist-mono",
+  display: "swap",
+  preload: true,
 });
